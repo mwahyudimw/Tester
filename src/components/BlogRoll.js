e9732d949f3afbf50755965c709f06efbc79691e
mwahyudim <wahyudi140701@gmail.com>
2020-09-11 13:46:07 +0700

    Update

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,104 +1,113 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql, StaticQuery } from 'gatsby';
+import PreviewCompatibleImage from './PreviewCompatibleImage';
+
+import '../components/all.sass';
 
 class BlogRoll extends React.Component {
-  render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+	render() {
+		const { data } = this.props;
+		const { edges: posts } = data.allMarkdownRemark;
 
-    return (
-      <div className="columns is-multiline">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
-                  </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </article>
-            </div>
-          ))}
-      </div>
-    )
-  }
+		return (
+			<div className="colums">
+				{posts &&
+					posts.map(({ node: post }) => (
+						<div className="column" key={post.id}>
+							<article className="media articleBlog">
+								<figure className="media-left">
+									{post.frontmatter.featuredimage ? (
+										<div className="featured-thumbnail">
+											<PreviewCompatibleImage
+												imageInfo={{
+													image: post.frontmatter.featuredimage,
+													alt: `featured image thumbnail for post ${post.frontmatter.title}`
+												}}
+											/>
+										</div>
+									) : null}
+								</figure>
+								<div className="media-content">
+									<div className="content">
+										<p>
+											<Link className="title has-text-dark is-size-5" to={post.fields.slug}>
+												{post.frontmatter.title}
+											</Link>
+											<p>{post.frontmatter.description}</p>
+											<p>{post.frontmatter.date}</p>
+										</p>
+									</div>
+									<nav className="level is-mobile">
+										<div className="level-left">
+											<a className="level-item">
+												<span className="icon is-small">
+													<i className="fas fa-reply" />
+												</span>
+											</a>
+											<a className="level-item">
+												<span className="icon is-small">
+													<i className="fas fa-retweet" />
+												</span>
+											</a>
+											<a className="level-item">
+												<span className="icon is-small">
+													<i className="fas fa-heart" />
+												</span>
+											</a>
+										</div>
+									</nav>
+								</div>
+							</article>
+						</div>
+					))}
+			</div>
+		);
+	}
 }
 
 BlogRoll.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
+	data: PropTypes.shape({
+		allMarkdownRemark: PropTypes.shape({
+			edges: PropTypes.array
+		})
+	})
+};
 
 export default () => (
-  <StaticQuery
-    query={graphql`
-      query BlogRollQuery {
-        allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-        ) {
-          edges {
-            node {
-              excerpt(pruneLength: 400)
-              id
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-                templateKey
-                date(formatString: "MMMM DD, YYYY")
-                featuredpost
-                featuredimage {
-                  childImageSharp {
-                    fluid(maxWidth: 120, quality: 100) {
-                      ...GatsbyImageSharpFluid
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={(data, count) => <BlogRoll data={data} count={count} />}
-  />
-)
+	<StaticQuery
+		query={graphql`
+			query BlogRollQuery {
+				allMarkdownRemark(
+					sort: { order: DESC, fields: [frontmatter___date] }
+					filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+				) {
+					edges {
+						node {
+							excerpt(pruneLength: 400)
+							id
+							fields {
+								slug
+							}
+							frontmatter {
+								title
+								templateKey
+								date(formatString: "MMMM DD, YYYY")
+								description
+								featuredpost
+								featuredimage {
+									childImageSharp {
+										fluid(maxWidth: 120, quality: 100) {
+											...GatsbyImageSharpFluid
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		`}
+		render={(data, count) => <BlogRoll data={data} count={count} />}
+	/>
+);
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,193 +1,140 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import Layout from '../components/Layout';
+import BlogHeading from '../components/BlogHeading';
+import BlogRoll from '../components/BlogRoll';
 
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => (
-  <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
-    </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
-)
+export const IndexPageTemplate = ({ image, title, heading, subheading, mainpitch, description, intro }) => (
+	<div>
+		<section className="section section--gradient">
+			<div className="container">
+				<div className="section">
+					<div className="columns is-multiline">
+						<div className="column is-12"> 
+							<BlogHeading />
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section className="section section--gradient">
+			<div className="container">
+				<div className="section">
+					<div className="columns is-multiline">
+						<div className="column is-7 is-offset-1">
+							<BlogRoll />
+						</div>
+						<div className="column is-4">
+							<div class="card">
+								<div class="card-content">
+									<div class="media">
+										<div class="media-left">
+											<figure class="image is-48x48">
+												<img
+													src="https://bulma.io/images/placeholders/96x96.png"
+													alt="Placeholder image"
+												/>
+											</figure>
+										</div>
+										<div class="media-content">
+											<p class="title is-4">John Smith</p>
+											<p class="subtitle is-6">@johnsmith</p>
+										</div>
+									</div>
+
+									<div class="content">
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis
+										mauris. <a>@bulmaio</a>.
+										<a href="#">#css</a> <a href="#">#responsive</a>
+										<br />
+										<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+);
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
+	image: PropTypes.oneOfType([ PropTypes.object, PropTypes.string ]),
+	title: PropTypes.string,
+	heading: PropTypes.string,
+	subheading: PropTypes.string,
+	mainpitch: PropTypes.object,
+	description: PropTypes.string,
+	intro: PropTypes.shape({
+		blurbs: PropTypes.array
+	})
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+	const { frontmatter } = data.markdownRemark;
 
-  return (
-    <Layout>
-      <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
-      />
-    </Layout>
-  )
-}
+	return (
+		<Layout>
+			<IndexPageTemplate
+				image={frontmatter.image}
+				title={frontmatter.title}
+				heading={frontmatter.heading}
+				subheading={frontmatter.subheading}
+				mainpitch={frontmatter.mainpitch}
+				description={frontmatter.description}
+				intro={frontmatter.intro}
+			/>
+		</Layout>
+	);
+};
 
 IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
+	data: PropTypes.shape({
+		markdownRemark: PropTypes.shape({
+			frontmatter: PropTypes.object
+		})
+	})
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
-      }
-    }
-  }
-`
+	query IndexPageTemplate {
+		markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+			frontmatter {
+				title
+				image {
+					childImageSharp {
+						fluid(maxWidth: 2048, quality: 100) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+				heading
+				subheading
+				mainpitch {
+					title
+					description
+				}
+				description
+				intro {
+					blurbs {
+						image {
+							childImageSharp {
+								fluid(maxWidth: 240, quality: 64) {
+									...GatsbyImageSharpFluid
+								}
+							}
+						}
+						text
+					}
+					heading
+					description
+				}
+			}
+		}
+	}
+`;
