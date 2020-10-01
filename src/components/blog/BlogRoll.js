ab6aa909377ab483fe4defe0920c57d460b01087
usamahbass <basalamahusamah7@gmail.com>
2020-10-01 11:39:24 +0700

    redesign folder structure

--- src/components/blog/BlogRoll.js
+++ src/components/blog/BlogRoll.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link, graphql, StaticQuery } from "gatsby";
 import { GoCalendar } from "react-icons/go";
-import PreviewCompatibleImage from "./PreviewCompatibleImage";
+import PreviewCompatibleImage from "../PreviewCompatibleImage";
 
 class BlogRoll extends React.Component {
   render() {
